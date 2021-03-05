e754ac40b8ddc492cc1661d07d0249974627032f
CeejayPrime <36557279+CeejayPrime@users.noreply.github.com>
2021-03-05 17:09:09 +0100

    Tank Volume Calculator
    new commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\chinonso iwuh\pycharmprojects\tankvolumecalc\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
